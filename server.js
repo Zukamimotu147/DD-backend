f55b4ff72f02b91442db6f7a32d4bfb1cbe146f2
Zuka <you@example.com>
2024-12-09 19:05:51 +0800

    gggg

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let users = [
   },
 ];
 
-const cloudinary = cloudinary.v2.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
